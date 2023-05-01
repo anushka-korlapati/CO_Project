2f91ee28474886f2afff653c42bbc4d5453bba98
anushka-k25 <anusha.korlapati@gmail.com>
2023-05-01 20:10:20 +0530

    a

--- Q1/anushka.py
+++ Q1/anushka.py
@@ -1,7 +1,6 @@
 reg_addr = {"R0":"000", "R1":"001", "R2":"010", "R3":"011", "R4":"100", "R5":"101", "R6":"110", "FLAGS":"111"}
-syntax="add R1 R2 R3"
-file_read_words=["add","R1","R2","R3"]
-
+# syntax="add R1 R2 R3"
+# file_read_words=["add","R1","R2","R3"]
 
 
 def typeA(opcode,file_read_words):
