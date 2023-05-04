370c2a0377da2f926d0e8f13197476c3ceb42908
Aditya Jagadale <jagadaleaditya057@gmail.com>
2023-05-04 15:11:11 +0530

    isa reg name

--- Q1/Adi.py
+++ Q1/Adi.py
@@ -1,22 +1,6 @@
 reg_addr = {"R0":"000", "R1":"001", "R2":"010", "R3":"011", "R4":"100", "R5":"101", "R6":"110", "FLAGS":"111"}
 
 #Type C
-count = 0
-def R_counter(file_read_words):
-    for i in file_read_words:
-        if i[0] == "R":
-            count += 1
-    return count
-count = R_counter()
-
-def Type_c(opcodes,file_read_words):
-    binary = opcodes + '00000'
-    for i in file_read_words:
-        temp_reg = ''
-        if i[0] == "R":
-            temp_reg = reg_addr[i]
-        binary += temp_reg
-    return binary
 
 
 def isa_reg_name_error(file_read_words):
